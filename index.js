548d4b8ab6eb404b6460faa322b7f4e1da68a487
Matthew Waters <matthewdw89@gmail.com>
2019-03-16 19:09:54 -0400

    First commit of socket.io. Have just set up folder structure and installed and setup socket.io in client and server sides

---
+++ index.js
@@ -0,0 +1,38 @@
+
+const express = require('express'),
+      bodyParser = require('body-parser'),
+      cors = require('cors'),
+      PORT = process.env.PORT || 8080;
+
+//App Setup
+    const app = express();
+    const server = app.listen(PORT, () => {
+        console.log(`Listening on ${PORT}`)
+    });
+
+// Middleware 
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// This could be a database, but I will just be using an array in this project
+let messageList = [];
+
+
+// Using our IO socket variable to now listen for connections
+
+io = require("socket.io")(server),
+
+io.on('connection', (socket) => {
+  console.log(`new connection: ${socket.id}`)
+  socket.on('start', () => {
+    io.emit('receiveNewMessage', messageList)
+  })
+
+  socket.on('sendNewMessage', (msg)=>{
+    messageList.push(msg)
+    io.emit('receiveNewMessage', messageList)
+  })
+
+  socket.on('disconnect', () => console.log("Client disconnected"))
+})
